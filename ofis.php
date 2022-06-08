9feafedc3380330b896e59a5e52a92781b4094db
sadettinwings <sadettin@wings.com.tr>
2022-06-08 20:48:59 +0300

    aa

---
+++ ofis.php
@@ -0,0 +1,23 @@
+<?php
+ function GetIP(){  
+            if(getenv("HTTP_CLIENT_IP")) {
+            $ip = getenv("HTTP_CLIENT_IP");
+            } elseif(getenv("HTTP_X_FORWARDED_FOR")) {
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+            if (strstr($ip, ',')) {
+                $tmp = explode (',', $ip);
+                $ip = trim($tmp[0]);
+            }
+            } else {
+            $ip = getenv("REMOTE_ADDR");
+            }
+            return $ip;
+           } 
+$ip = GetIP();
+echo $ip;
+
+
+
+
+
+?>
